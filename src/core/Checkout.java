7ce2fe9de6f20733b0953bce2532dc6be53754f1
anjali <anjali.sreyas.r@gmail.com>
2018-11-10 01:53:23 +1100

    Integrated rule processing into Checkout

--- src/core/Checkout.java
+++ src/core/Checkout.java
@@ -24,11 +24,51 @@ public class Checkout {
 
     public BigDecimal total(){
         BigDecimal total = new BigDecimal(0);
+        Map<Item, Integer> processedItems = new HashMap<>();
+        Map<Item, Integer> freeItems = new HashMap<>();
 
-        for (Map.Entry<Item, Integer> entry : cartContents.entrySet()) {
+        for (IPricingRule pricingRule : pricingRules) {
+            PricingRuleResponse response = pricingRule.evaluate(cartContents);
+            total = total.add(response.getTotalPrice());
+            mergeMap(response.getProcessedItems(), processedItems);
+            mergeMap(response.getFreeItems(), freeItems);
+        }
+
+        Map<Item,Integer> pending = getPendingItems(cartContents, processedItems);
+
+        for (Map.Entry<Item, Integer> entry : pending.entrySet()) {
             total = total.add(entry.getKey().getPrice().multiply(new BigDecimal(entry.getValue())));
         }
 
+        mergeMap(freeItems, cartContents);
+
         return total;
+    }
+
+    public Map<Item, Integer> getCartContents() {
+        return cartContents;
+    }
+
+    private void mergeMap(Map<Item, Integer> source, Map<Item, Integer> target)
+    {
+        for (Map.Entry<Item, Integer> sourceKvp : source.entrySet()) {
+            if (target.containsKey(sourceKvp.getKey())){
+                target.put(sourceKvp.getKey(), target.get(sourceKvp.getKey()) + sourceKvp.getValue());
+            }
+            else {
+                target.put(sourceKvp.getKey(), sourceKvp.getValue());
+            }
+        }
+    }
+
+    private Map<Item, Integer> getPendingItems(Map<Item, Integer> cart, Map<Item, Integer> processedItems)
+    {
+        Map<Item, Integer> pending = new HashMap<>(cart);
+
+        for (Map.Entry<Item, Integer> processedKvp : processedItems.entrySet()) {
+            pending.put(processedKvp.getKey(), cart.get(processedKvp.getKey()) - processedKvp.getValue());
+        }
+
+        return pending;
     }
 }
--- test/CheckoutTest.java
+++ test/CheckoutTest.java
@@ -1,10 +1,13 @@
 import core.Catalogue;
 import core.Checkout;
+import core.IPricingRule;
 import core.Item;
 import org.junit.jupiter.api.Test;
+import rules.BuyMPayForN;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,6 +22,44 @@ class CheckoutTest {
         Checkout checkout = new Checkout(new ArrayList<>());
         checkout.scan(catalogue.findProduct("vga"));
         checkout.scan(catalogue.findProduct("vga"));
+        checkout.scan(catalogue.findProduct("vga"));
+        assertEquals(0, checkout.total().compareTo(new BigDecimal(90.0)));
+    }
+
+    @Test
+    void BuyMPayForNOfferIntegrationTest() {
+
+        Catalogue catalogue = new Catalogue();
+        Item vga = new Item("vga",new BigDecimal(30),"VGA Adapter");
+        catalogue.addProduct(vga);
+
+        List<IPricingRule> pricingRules = new ArrayList<IPricingRule>();
+        pricingRules.add(new BuyMPayForN(vga, 3, 2));
+        Checkout checkout = new Checkout(pricingRules);
+
+        checkout.scan(catalogue.findProduct("vga"));
+        checkout.scan(catalogue.findProduct("vga"));
+        checkout.scan(catalogue.findProduct("vga"));
+
         assertEquals(0, checkout.total().compareTo(new BigDecimal(60.0)));
+    }
+
+    @Test
+    void BuyMPayForNOfferWithSpareIntegrationTest() {
+
+        Catalogue catalogue = new Catalogue();
+        Item vga = new Item("vga",new BigDecimal(30),"VGA Adapter");
+        catalogue.addProduct(vga);
+
+        List<IPricingRule> pricingRules = new ArrayList<IPricingRule>();
+        pricingRules.add(new BuyMPayForN(vga, 3, 2));
+        Checkout checkout = new Checkout(pricingRules);
+
+        checkout.scan(catalogue.findProduct("vga"));
+        checkout.scan(catalogue.findProduct("vga"));
+        checkout.scan(catalogue.findProduct("vga"));
+        checkout.scan(catalogue.findProduct("vga"));
+
+        assertEquals(0, checkout.total().compareTo(new BigDecimal(90.0)));
     }
 }
